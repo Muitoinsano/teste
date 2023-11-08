efc2cfa9b84f151c59c71efc1d0087f4f53951eb
Muitoinsano <150296618+Muitoinsano@users.noreply.github.com>
2023-11-08 18:46:48 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -4,27 +4,32 @@ const path = require('path');
 
 // Carregue o arquivo JSON
 const jsonFile = fs.readFileSync(path.join(__dirname, 'file.json'));
-const jsonData = JSON.parse(jsonFile).Parametros; // Acessando a propriedade 'Parametros'
+const jsonData = JSON.parse(jsonFile).Parametros;
 
 // Carregue o arquivo YAML
 const yamlFile = fs.readFileSync(path.join(__dirname, 'file.yml'), 'utf8');
 let yamlData = yaml.load(yamlFile);
 
-// Percorra cada linha do arquivo YAML
-for (let key in yamlData) {
-    if (typeof yamlData[key] === 'string' && yamlData[key].includes('${')) {
-        // Encontre a variável de ambiente
-        const envVar = yamlData[key].match(/\${(.*)}/)[1];
+// Função para percorrer todos os nós do objeto YAML
+function replaceEnvVars(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] === 'string' && obj[key].includes('${')) {
+            // Encontre a variável de ambiente
+            const envVar = obj[key].match(/\${(.*)}/)[1];
 
-        // Substitua pela propriedade correspondente do arquivo JSON
-        if (jsonData.hasOwnProperty(envVar)) {
-            yamlData[key] = yamlData[key].replace(`\${${envVar}}`, jsonData[envVar]);
+            // Substitua pela propriedade correspondente do arquivo JSON
+            if (jsonData.hasOwnProperty(envVar)) {
+                obj[key] = obj[key].replace(`\${${envVar}}`, jsonData[envVar]);
+            }
+        } else if (typeof obj[key] === 'object') {
+            // Se o valor for um objeto, percorra seus nós
+            replaceEnvVars(obj[key]);
         }
     }
 }
 
+// Chame a função no objeto YAML
+replaceEnvVars(yamlData);
+
 // Escreva o YAML atualizado de volta para o arquivo
 fs.writeFileSync(path.join(__dirname, 'file.yml'), yaml.dump(yamlData));
-
-
-//npm install js-yaml
